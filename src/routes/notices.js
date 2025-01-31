9aa0527edbf3656de36cf5cc080e05be409a1c82
deadshoot333 <arqam343almas@gmail.com>
2025-01-31 19:12:59 +0600

    Notice Backend functionality added

--- src/routes/notices.js
+++ src/routes/notices.js
@@ -12,73 +12,150 @@ router.get('/',async(req,res)=>{
     }
 })
 router.post('/', async (req, res) => {
-    const { teacher_id, title, content } = req.body;
-    console.log(`Teacher id:${teacher_id} Title:${title} content:${content}`)
-    try {
-        const { data,error } = await supabase
-            .from('notices')
-            .insert({ teacher_id, title, content })
-            .select();
-
-        if (error) return res.status(500).json({ error: error.message });
-
-        res.status(201).json({ message: 'Notice created successfully',data });
-    } catch (err) {
-        res.status(500).json({ error: 'Internal server error' });
+    const { admin_id,teacher_id, title, content,type } = req.body;
+    if (!admin_id)
+    {
+        console.log(`Add Notice Teacher id:${teacher_id} Title:${title} content:${content} type:${type}`)
+        try {
+            const { data,error } = await supabase
+                .from('notices')
+                .insert({ teacher_id, title, content })
+                .select();
+    
+            if (error) return res.status(500).json({ error: error.message });
+    
+            res.status(201).json({ message: 'Notice created successfully',data });
+        } catch (err) {
+            res.status(500).json({ error: 'Internal server error' });
+        }    
     }
+    else{
+        console.log(`Add Notice Admin id:${admin_id} Title:${title} content:${content} type:${type}`)
+        try {
+            const { data,error } = await supabase
+                .from('notices')
+                .insert({ admin_id, title, content,type })
+                .select();
+    
+            if (error) return res.status(500).json({ error: error.message });
+    
+            res.status(201).json({ message: 'Notice created successfully',data });
+        } catch (err) {
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    }
+    
 });
 router.put('/:id', async (req, res) => {
     const { id } = req.params;
-    const { teacherId, title, content } = req.body;
-
-    try {
-        const { data: notice, error: fetchError } = await supabase
-            .from('notices')
-            .select('teacher_id')
-            .eq('id', id)
-            .single();
-
-        if (fetchError || notice.teacher_id !== teacherId) {
-            return res.status(403).json({ error: 'Unauthorized' });
+    const { adminId,teacherId,content } = req.body;
+    if(!adminId)
+    {
+        console.log(`Edit Notice Teacher Id:${teacherId} Edited Content:${content}`)
+        try {
+            const { data: notice, error: fetchError } = await supabase
+                .from('notices')
+                .select('teacher_id')
+                .eq('notice_id', id)
+                .single();
+    
+            if (fetchError || notice.teacher_id !== teacherId) {
+                return res.status(403).json({ error: 'Unauthorized' });
+            }
+    
+            const { error } = await supabase
+                .from('notices')
+                .update({content})
+                .eq('notice_id', id);
+    
+            if (error) return res.status(500).json({ error: error.message });
+    
+            res.status(200).json({ message: 'Notice updated successfully' });
+        } catch (err) {
+            res.status(500).json({ error: 'Internal server error' });
         }
-
-        const { error } = await supabase
-            .from('notices')
-            .update({ title, content })
-            .eq('id', id);
-
-        if (error) return res.status(500).json({ error: error.message });
-
-        res.status(200).json({ message: 'Notice updated successfully' });
-    } catch (err) {
-        res.status(500).json({ error: 'Internal server error' });
+    }
+    else{
+        console.log(`Edit Notice Admin Id:${adminId} Edited Content:${content}`)
+        try {
+            const { data: notice, error: fetchError } = await supabase
+                .from('notices')
+                .select('admin_id')
+                .eq('notice_id', id)
+                .single();
+    
+            if (fetchError || notice.admin_id !== adminId) {
+                return res.status(403).json({ error: 'Unauthorized' });
+            }
+    
+            const { error } = await supabase
+                .from('notices')
+                .update({content})
+                .eq('notice_id', id);
+    
+            if (error) return res.status(500).json({ error: error.message });
+    
+            res.status(200).json({ message: 'Notice updated successfully' });
+        } catch (err) {
+            res.status(500).json({ error: 'Internal server error' });
+        }
     }
 });
 router.delete('/:id', async (req, res) => {
     const { id } = req.params;
-    const { teacherId } = req.body;
-    console.log(`TeacherId: ${teacherId} NoticeId:${id}`)
-    try {
-        const { data: notice, error: fetchError } = await supabase
-            .from('notices')
-            .select('teacher_id')
-            .eq('notice_id', id)
-            .single();
-
-        if (fetchError || notice.teacher_id !== teacherId) {
-            return res.status(403).json({ error: 'Unauthorized' });
+    const { teacherId,adminId } = req.body;
+    if(!adminId)
+    {
+        console.log(`Delete Notice TeacherId: ${teacherId} NoticeId:${id}`)
+        try {
+            const { data: notice, error: fetchError } = await supabase
+                .from('notices')
+                .select('teacher_id')
+                .eq('notice_id', id)
+                .single();
+    
+            if (fetchError || notice.teacher_id !== teacherId) {
+                return res.status(403).json({ error: 'Unauthorized' });
+            }
+    
+            const { error } = await supabase
+                .from('notices')
+                .delete()
+                .eq('notice_id', id);
+    
+            if (error) return res.status(500).json({ error: error.message });
+    
+            res.status(200).json({ message: 'Notice deleted successfully' });
+        } catch (err) {
+            res.status(500).json({ error: 'Internal server error' });
         }
 
-        const { error } = await supabase
-            .from('notices')
-            .delete()
-            .eq('notice_id', id);
-
-        if (error) return res.status(500).json({ error: error.message });
-
-        res.status(200).json({ message: 'Notice deleted successfully' });
-    } catch (err) {
-        res.status(500).json({ error: 'Internal server error' });
+    }
+    else
+    {
+        console.log(`Delete Notice AdminId: ${adminId} NoticeId:${id}`)
+        try {
+            const { data: notice, error: fetchError } = await supabase
+                .from('notices')
+                .select('admin_id')
+                .eq('notice_id', id)
+                .single();
+    
+            if (fetchError || notice.admin_id !== adminId) {
+                return res.status(403).json({ error: 'Unauthorized' });
+            }
+    
+            const { error } = await supabase
+                .from('notices')
+                .delete()
+                .eq('notice_id', id);
+    
+            if (error) return res.status(500).json({ error: error.message });
+    
+            res.status(200).json({ message: 'Notice deleted successfully' });
+        } catch (err) {
+            res.status(500).json({ error: 'Internal server error' });
+        } 
     }
 });
 
