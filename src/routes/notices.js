56bed34dbf69c1c61e7570ecca77b5df4b129f9e
deadshoot333 <arqam343almas@gmail.com>
2025-02-16 08:54:34 +0600

    nothing important

--- src/routes/notices.js
+++ src/routes/notices.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const router=express.Router()
 const supabase=require('../../connection')
-
+//
 router.get('/',async(req,res)=>{
     try {
         const{data,error}=await supabase.from("notices").select("*");
