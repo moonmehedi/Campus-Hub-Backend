41ab462c06cb28f5aa4c66760840fc979e5a1c0d
moonmehedi <the.mehedi.hasan.moon@gmail.com>
2025-02-21 14:47:38 +0600

    added admin log in

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,8 +4,6 @@ const requireAuth = (req, res, next) => {
     const timestamp = getCurrentTimestamp();
     console.log("Session Data:", req.session);
 
-
-
     // Set `req.currentUser` based on the session type
     if (req.session.teacher) {
         console.log(`[${timestamp}] Teacher ID ${req.session.teacher.teacher_id} authenticated`);
@@ -14,7 +12,7 @@ const requireAuth = (req, res, next) => {
             id: req.session.teacher.teacher_id,
             name: req.session.teacher.name,
             timestamp
-        };
+        }
     } else if (req.session.student) {
         console.log(`[${timestamp}] Student ID ${req.session.student.student_id} authenticated`);
         req.currentUser = {
@@ -22,11 +20,19 @@ const requireAuth = (req, res, next) => {
             id: req.session.student.student_id,
             name: req.session.student.name,
             timestamp
-        };
-    }    else {
+        }
+    } else if (req.session.admin) {
+        console.log(`[${timestamp}] Admin ID ${req.session.admin.email} authenticated`);
+        req.currentUser = {
+            type: "admin",
+            id: req.session.admin.email,
+            name: req.session.admin.name,
+            timestamp
+        }
+    } else {
         return res.status(401).json({ 
             success: false, 
-            message: 'Authentication required',
+            message: 'Authentication required !',
             timestamp
         });
     }
