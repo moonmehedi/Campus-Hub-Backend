93a76a2535446cbb21824193bb807c2eb34c973d
Sadia-Jahan-Moon <97521796+Sadia-Jahan-Moon@users.noreply.github.com>
2025-02-21 20:09:05 +0600

    all done att

--- src/routes/courseadvisor.js
+++ src/routes/courseadvisor.js
@@ -19,6 +19,36 @@ router.get("/all-result/:student_id", async (req, res) => {
         res.status(500).json({ success: false, message: "Error fetching courses", error: err.message });
     }
   });
+  router.get("/attendance/:student_id", async (req, res) => {
+    const { student_id } = req.params;
+    try {
+        const { data, error } = await supabase
+            .from("attendance")
+            .select("date, class_period, course_code, present, remark") // Fetch related course details
+            .eq("student_id", student_id)
+            .eq("course_code", "309") // Filter by course_code 309
+            .order("date", { ascending: true })
+            .order("class_period", { ascending: true });
+            
+  
+        if (error) throw error;
+  
+        // Format the response properly
+        const formattedData = data.map(item => ({
+            date: item.date,
+            class_period: item.class_period,
+            course_code: item.course_code,
+            present: item.present,
+            remark:item.remark
+        }));
+  
+        res.status(200).json({ success: true, data: formattedData });
+    } catch (err) {
+        console.error("Error fetching courses for improvement:", err.message);
+        res.status(500).json({ success: false, message: "Error fetching courses", error: err.message });
+    }
+  });
+
 router.get("/courses-to-improve/:student_id", async (req, res) => {
     const { student_id } = req.params;
     try {
